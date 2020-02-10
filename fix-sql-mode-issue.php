cb88f7e113031bd36ccb094c2c120a54b87ff852
Raphael Batagini <raphabatagini@gmail.com>
2020-02-10 17:58:28 -0300

    Change plugin name, description and main file name

--- fix-sql-mode-issue.php
+++ fix-sql-mode-issue.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package     Fix_MySQL_8_Issue
+ * @package     Fix_SQL_Mode_Issue
  * @author      Studio Visual
  * @license     GPL-2.0+
  *
- * Plugin Name: Fix Mysql 8 Issue
+ * Plugin Name: Fix SQL Mode Issue
  * Plugin URI: https://studiovisual.com.br
- * Description: Solves the error "ORDER BY clause is not in SELECT list" presented in MySQL 8+
+ * Description: Solves the error "ORDER BY clause is not in SELECT list" presented by MySQL in WordPress because of sql_mode misconfiguration.
  * Version: 0.1.0
  * Author: Studio Visual
  * Author URI: https://studiovisual.com.br
